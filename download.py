e4338f2072179fbd0e108c17304fd1928336a4e9
Jonahvem <119072632+Jonahvem@users.noreply.github.com>
2026-05-01 18:22:11 +0200

    add files

---
+++ download.py
@@ -0,0 +1,3 @@
+def download_song(song_url):
+    
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+import download  
+
+if __name__ == "__main__":
+	print("Running download.py")
+	# Example: If download.py has a function named `start_download`, call it here
+	# download.start_download()
