5bff661acb07ca6f374dcb77adb21ff64081d3ff
Jonahvem <119072632+Jonahvem@users.noreply.github.com>
2026-05-01 18:39:17 +0200

    save progress

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Spotify-mp3-download
 This repository contains code to download entire Spotify playlists or single songs
+- Install requirements before using script
--- download.py
+++ download.py
@@ -1,3 +1,11 @@
-def download(song_url):
+import subprocess
+import os
+
+def download():
+    
+    url = input("Enter the URL to download: ")
+    output_path = input("Enter the output path (including filename): ")
+
+    subprocess.run
 
     
